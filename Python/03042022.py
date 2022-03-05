ba14126c53621449095ea724e7bb4a2c8e31073b
Joy Lee <joy109lee@gmail.com>
2022-03-05 15:00:58 -0800

    answer

--- Python/03042022.py
+++ Python/03042022.py
@@ -35,7 +35,8 @@ P.S. The situation in this kata can be likened to the more-computer-science-rela
 
 
 def queue_time(customers, n):
-    if len(customers) <= 2:
-        return max(customers)
-    if n == 1:
-        return sum(customers)
+    till = [0] * n
+    for customer in customers:
+        till[0] += customer
+        till.sort()
+    return max(till)
