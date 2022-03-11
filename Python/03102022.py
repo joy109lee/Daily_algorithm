fbab40e294be84111cd98d4529415a6096d9ba7b
Joy Lee <joy109lee@gmail.com>
2022-03-10 16:26:21 -0800

    duplicate_encode

--- Python/03102022.py
+++ Python/03102022.py
@@ -9,3 +9,5 @@
 # "(( @"     =>  "))(("
 # Notes
 # Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+def duplicate_encode(word):
+    # your code here
